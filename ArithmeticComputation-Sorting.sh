2705c01ce3b78325c5e848612881d5204142126e
Mr-Abhi1289 <abhimvj575@gmail.com>
2022-11-24 15:56:16 +0530

    Added: Read the values from the Dictionary into the array

--- ArithmeticComputation-Sorting.sh
+++ ArithmeticComputation-Sorting.sh
@@ -1,22 +1,31 @@
+
 declare -A arithmaticOperation
 
 read -p "Enter first input :" firstInput
 read -p "Enter second input:" secondInput
 read -p "Enter third input :" thirdInput
-result=`echo "scale=2;$firstInput + $secondInput * $thirdInput" | bc`
-echo "Result:" $result
-
-resultOne=`echo "scale=2;$firstInput * $secondInput + $thirdInput" | bc`
-echo "Result:" $resultOne
-
-resultTwo=`echo "scale=2;$thirdInput + $firstInput / $secondInput " | bc`
-echo "Result:" $resultTwo
-
-resultThree=`echo "scale=2;$firstInput % $secondInput + $thirdInput" | bc`
-echo "Result:" $resultThree
 
 
-arithmaticOperation[result]=$result
-arithmaticOperation[resultOne]=$resultOne
-arithmaticOperation[resultTwo]=$resultTwo
-arithmaticOperation[resultThree]=$resultThree
+result1=`echo "scale=2;$firstInput + $secondInput * $thirdInput" | bc`
+echo "Result:" $result1
+
+result2=`echo "scale=2;$firstInput * $secondInput + $thirdInput" | bc`
+echo "Result:" $result2
+
+result3=`echo "scale=2;$thirdInput + $firstInput / $secondInput " | bc`
+echo "Result:" $result3
+
+result4=`echo "scale=2;$firstInput % $secondInput + $thirdInput" | bc`
+echo "Result:" $result4
+
+arithmaticOperation[result1]=$result1
+arithmaticOperation[result2]=$result2
+arithmaticOperation[result3]=$result3
+arithmaticOperation[result4]=$result4
+
+
+for((index=0; index<=${#arithmaticOperation[@]}; index++))
+do
+  array[index]=${arithmaticOperation[result$((index+1))]}
+done
+echo "${array[@]}"
